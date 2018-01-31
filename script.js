e147e2f69dcad4754e29c933b11bd5d8810557c4
monika0711 <35463805+monika0711@users.noreply.github.com>
2018-01-31 20:54:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function show (x){
+calc.score.value += x;
+}
+
+function showminus()
+{
+calc.score.value += "-";
+}
+
+function showplus()
+{
+calc.score.value += "+";
+}
+
+function showstar()
+{
+calc.score.value += "*";
+}
+
+function showdivision()
+{
+calc.score.value += "/";
+}
+
